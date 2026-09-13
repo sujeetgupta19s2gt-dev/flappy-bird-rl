db0596f2b155774f28ab8da05f76c8f9e8392ef0
sujeetgupta19s2gt-dev <sujeetgupta19s2gt@gmail.com>
2026-09-13 23:28:53 +0530

    Add files via upload

---
+++ experience_replay.py
@@ -0,0 +1,19 @@
+from collections import deque
+import random
+
+class ReplayMemory():
+    # create FIFO queue
+    def __init__(self, maxlen, seed=None):
+        if seed is not None:
+            random.seed(seed)
+        self.memory = deque([], maxlen=maxlen)
+
+    def append(self, new_exp):
+        self.memory.append(new_exp)
+
+    def sample(self, sample_size):
+        return random.sample(self.memory, sample_size)
+
+    # curr buffer size
+    def __len__(self):
+        return len(self.memory)
